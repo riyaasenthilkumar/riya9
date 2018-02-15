f788e2a08265168fba171ad9e612b068c62ca7fb
riyaasenthilkumar <36299182+riyaasenthilkumar@users.noreply.github.com>
2018-02-15 12:17:30 +0530

    Create integer.py

---
+++ integer.py
@@ -0,0 +1,10 @@
+n=int(raw_input("enter the values"))
+a=int(raw_input("Enter the values"))
+ll=[]
+s=0
+for i in range(0,n):
+    l=int(raw_input("enter the values"))
+    ll.append(l)
+for i in range(0,n):
+    s+=int(ll)
+    print(s)
